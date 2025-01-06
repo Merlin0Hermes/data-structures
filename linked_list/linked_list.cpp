f5f1d5df4dd6dad63b678a141670294f73c2f3ba
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-01-06 21:03:04 +0530

    add basic interface in main function to interact with linked list

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -60,10 +60,40 @@ void print_list(Node *head)
 int main()
 {
 	Node *list = nullptr;
+	
+	int option{};
 
-	for (Node *temp = list; temp != nullptr; temp = temp->next)
+	std::cout << "Operations\n\n";
+	std::cout << "0: exit.\n";
+	std::cout << "1: Insert an element.\n";
+	std::cout << "2: Print the list.\n";
+	std::cout << "3: Delete the list.\n\n";
+	
+	int val;
+	while (true) 
 	{
-		std::cout << temp->data << " ";
+		std::cout << "Enter the operation: ";
+		std::cin >> option;
+		switch (option)
+		{
+			case 0:
+				exit(0);
+				break;
+			case 1:
+				std::cout << "Enter an element: "; 
+				std::cin >> val;
+				insert_end(list, val);
+				break;
+			case 2: 
+				print_list(list); 
+				break;
+			case 3: 
+				delete_list(list); 
+				break;
+			default: 
+				std::cerr << "Error: Invalid operation.\n";
+		}
+	
 	}
 
 	delete_list(list);
