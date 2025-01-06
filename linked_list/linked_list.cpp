e332f8f6e8447609f97735c18e7625f78715d743
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-01-06 20:25:27 +0530

    add function to insert to end of linked list

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -6,7 +6,29 @@ typedef struct Node
 	Node *next;
 };
 
+void insert_end(Node*& head, int value)
+{
+	Node *new_node = new Node;
+	new_node->next = nullptr;
+	new_node->data = value;
+
+	if (head == nullptr)
+	{
+		head = new_node;
+		return;
+	}
+
+	Node *temp = head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new_node;
+}
+
 int main()
 {
+	Node *head = NULL;
+
 	return 0;
 }
