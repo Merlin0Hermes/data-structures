96b405c6f952860b6a89c5617460bf2994f341ff
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-07-20 17:11:47 +0530

    add a swap function

--- selection_sort/Sort.java
+++ selection_sort/Sort.java
@@ -5,8 +5,15 @@ public class Sort {
     public static void sort(int[] arr) {
         
         for (int i = 0; i < arr.length; ++i) {
-
+            int smallestIndex = findSmallest(arr, i);
+            
         }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     private static int findSmallest(int[] arr, int start) {
