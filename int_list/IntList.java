80f0ca8b8e0894aabf0aa2cc08f8aa27780561c7
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-07-20 20:01:57 +0530

    add size function

--- int_list/IntList.java
+++ int_list/IntList.java
@@ -7,9 +7,17 @@ public class IntList {
         rest = r;
     }
 
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + rest.size();
+    }
+
     public static void main(String[] args) {
         IntList list = new IntList(5, null);
         list = new IntList(10, list);
         list = new IntList(15, list);
+        System.out.println(list.size());
     }
 }
