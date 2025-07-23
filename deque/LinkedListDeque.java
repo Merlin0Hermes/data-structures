65567045c0005d6b9d422eb13d954e747a80f684
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-07-23 18:15:46 +0530

    add constructors

--- deque/LinkedListDeque.java
+++ deque/LinkedListDeque.java
@@ -8,5 +8,35 @@ public class LinkedListDeque<T> {
         Node<T> prev;
     }
 
+    /** first item in list is at sentinel.next (if it exists) */
+    Node<T> sentinel;
+    int size;
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new Node<T>();
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+
+    }
+
+    public LinkedListDeque(T item) {
+        size = 1;
+        sentinel = new Node<T>();
+        Node<T> node = new Node<T>();
+
+        sentinel.next = node;
+        sentinel.prev = node;
+
+        node.prev = sentinel;
+        node.next = sentinel;
+
+        node.item = item;
+    }
+
+    public static void main(String[] args) {
+        LinkedListDeque<Integer> list = new LinkedListDeque<>(5);
+
+    }
 
 }
