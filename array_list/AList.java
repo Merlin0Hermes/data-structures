eec22895f376852eace48aab0f50d22f1ac89123
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-07-23 20:17:16 +0530

    add AList class methods

--- array_list/AList.java
+++ array_list/AList.java
@@ -1,3 +1,31 @@
-public class AList {
+package array_list;
 
+public class AList {
+    int[] items;
+    int size;
+
+    public AList() {
+        items = new int[100];
+        size = 0;
+    }
+
+    public void addLast(int item) {
+        items[size++] = item;
+    }
+
+    public int getLast() {
+        return items[size - 1];
+    }
+
+    public int get(int i) {
+        return items[i];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
