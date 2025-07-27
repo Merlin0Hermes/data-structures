53442cc4ce8d151aef08856934040ba375b18eb4
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-07-27 20:38:36 +0530

    add longest() function

--- word_utils/WordUtils.java
+++ word_utils/WordUtils.java
@@ -1,3 +1,22 @@
+package word_utils;
+
+import deque.LinkedListDeque;
+import array_list.AList;
+
 public class WordUtils {
+
+    public static String longest(LinkedListDeque<String> list) {
+        int maxIndex = 0;
+        for (int i = 0; i < list.size(); ++i) {
+            String maxString = list.get(maxIndex);
+            String thisString = list.get(i);
+            if (thisString.length() > maxString.length()) {
+                maxIndex = i;
+            }
+        }
+        return list.get(maxIndex);
+    }
+
+
 
 }
