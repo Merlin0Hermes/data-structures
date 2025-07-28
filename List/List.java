040e5f0b172dba0f7761a0359e97a077e7c3e0e1
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-07-28 15:36:17 +0530

    add interface

---
+++ List/List.java
@@ -0,0 +1,14 @@
+package List;
+
+import deque.LinkedListDeque;
+import array_list.AList;
+
+public interface List<T> {
+    public void addFirst(T item);
+    public void addLast(T item);
+    public T getFirst();
+    public T getLast();
+    public T removeLast();
+    public T get(int i);
+    public int size();
+}
