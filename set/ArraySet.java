39e2ca3711e7be7b77d0c369ee5354e84545c19b
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-08-02 16:34:51 +0530

    add basic ArraySet

--- set/ArraySet.java
+++ set/ArraySet.java
@@ -1,25 +1,34 @@
 package set;
 
 public class ArraySet<T> {
+    private T[] items;
+    private int size;
 
     public ArraySet() {
+        items = (T[]) new Object[8];
+        size = 0;
     }
 
     /* Returns true if this map contains a mapping for the specified key.
      */
     public boolean contains(T x) {
+        for (int i = 0; i < size(); ++i) {
+            if (x == items[i]) {
+                return true;
+            }
+        }
         return false;
     }
 
     /* Associates the specified value with the specified key in this map.
        Throws an IllegalArgumentException if the key is null. */
     public void add(T x) {
-        return;
+        items[size++] = x;
     }
 
     /* Returns the number of key-value mappings in this map. */
     public int size() {
-        return 0;
+        return size;
     }
 
     public static void main(String[] args) {
