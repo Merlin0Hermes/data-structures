c4b8ea6316c607696e9281b3f158605bc270d07d
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-08-03 16:37:14 +0530

    remove unnecessary size variable

--- stack/Stack.java
+++ stack/Stack.java
@@ -4,16 +4,17 @@ import deque.LinkedListDeque;
 
 public class Stack<T>{
     private LinkedListDeque<T> stack;
-    private int size;
 
     public Stack() {
         stack = new LinkedListDeque<>();
-        ++size;
     }
     public void push(T item) {
         stack.addLast(item);
     }
     public T pop() {
         return stack.removeLast();
+    }
+    public int size() {
+        return stack.size();
     }
 }
